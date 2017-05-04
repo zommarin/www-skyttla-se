6cba0cd6683e75911abb1e2f138cf76c7bd6a0ed
Anders Zommarin <anders.zommarin@netinsight.net>
2017-05-04 08:54:58 +0200

    Snapshot from one.com

---
+++ aspnet_client/system_web/1_1_4322/_vti_cnf/smartnav.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|18 Feb 2005 16:33:18 -0000
+vti_author:SR|skyttla.se
+vti_modifiedby:SR|skyttla.se
+vti_nexttolasttimemodified:TR|18 Feb 2005 16:33:18 -0000
+vti_timecreated:TR|25 Jun 2006 08:23:30 -0000
+vti_extenderversion:SR|5.0.2.2634
+vti_syncwith_localhost\\\\\\www\\work\\web\\www.skyttla.org///www/work/web/www.skyttla.org:TR|18 Feb 2005 16:33:18 -0000
+vti_cacheddtm:TX|25 Jun 2006 08:23:30 -0000
+vti_filesize:IR|9427
+vti_backlinkinfo:VX|
---
+++ aspnet_client/system_web/1_1_4322/_vti_cnf/webuivalidation.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|18 Feb 2005 16:33:34 -0000
+vti_author:SR|skyttla.se
+vti_modifiedby:SR|skyttla.se
+vti_nexttolasttimemodified:TR|18 Feb 2005 16:33:34 -0000
+vti_timecreated:TR|25 Jun 2006 08:23:31 -0000
+vti_extenderversion:SR|5.0.2.2634
+vti_syncwith_localhost\\\\\\www\\work\\web\\www.skyttla.org///www/work/web/www.skyttla.org:TR|18 Feb 2005 16:33:34 -0000
+vti_cacheddtm:TX|25 Jun 2006 08:23:31 -0000
+vti_filesize:IR|14482
+vti_backlinkinfo:VX|
